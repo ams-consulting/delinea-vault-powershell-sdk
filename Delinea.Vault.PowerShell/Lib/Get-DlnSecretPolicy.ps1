16fb0755e1ccef7048c8299dfbbc1690a12c5369
Helmira38 <viguier.cathy@gmail.com>
2026-03-03 11:16:26 +0000

    modification doc Dln Secret policy

--- Delinea.Vault.PowerShell/Lib/Get-DlnSecretPolicy.ps1
+++ Delinea.Vault.PowerShell/Lib/Get-DlnSecretPolicy.ps1
@@ -29,13 +29,28 @@
 
 <#
 .SYNOPSIS
-This Cmdlet retrieves important information about User(s) on the system.
+This Cmdlet retrieves important information about SecretPolicy on the system.
 
 .DESCRIPTION
-This Cmdlet retrieves important information about User(s) on the system. Can return a single user by specifying the Username.
+This Cmdlet retrieves important information about SecretPolicy on the system. 
 
-.PARAMETER Name
-Specify the User by its username.
+.PARAMETER Id
+Specify the object by ID to get all details.
+
+.PARAMETER SearchText
+Specify the search text to use to filter results.
+
+.PARAMETER PageSize 
+Specify the number of objects by pages when using pagination (default is 100).
+
+.PARAMETER IncludeInactive
+Specify if results should include inactive users (default is false).
+
+.PARAMETER SortDirection
+Specify the sorting direction between 'Asc' and 'Desc' (default is 'Asc').
+
+.PARAMETER SortBy
+Specify the field to sort by (default is 'name').
 
 .INPUTS
 None
